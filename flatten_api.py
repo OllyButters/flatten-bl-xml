ce3dd828e19d73382fe76c72a1ebe399212c6f0c
Olly Butters <olly.butters@bristol.ac.uk>
2015-06-20 15:35:47 +0100

    added upload and import

--- flatten_api.py
+++ flatten_api.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 #Olly Butters
-#19/6/15
+#20/6/15
 
 #First stab at taking one of the BL XML files and flattening it into a CSV file to 
 #push into opal.
@@ -27,40 +27,40 @@ import json
 import subprocess
 import time
 
+#opal project that tables get put into
+project = 'bl'
+
 #Make a temp directory to put all the temp JSON and CSV files.
 #os.mkdir('temp')
 root_dir = os.getcwd()
 
+#List of all the books to process
 books = ['hamlet']
 
 
 for this_book in books:
     ########################
     #Set up the table first
-    
+    print 'Setting up the table'
+
     table_name = this_book
         
     #Table definition
     this_table_dic = {'entityType': 'Book', 'name': table_name}
 
     #save it as a json file
-    this_table_json_name = 'temp/'+table_name+'.json'
-    #this_table_json_name = 'my_table.json'
+    this_table_json_name = 'temp/'+table_name+'_table.json'
     with open(this_table_json_name, 'w') as fp:
         json.dump(this_table_dic, fp, indent=4)
         
     #Push the above json file into the opal API, this will make an empty table i.e. no variables.
-    #cmd = 'opal rest -o http://192.168.56.101:8080 -u administrator -p password -m POST -ct "application/json" /datasource/bl/tables < '+this_table_json_name
-    #cmd = 'opal rest -o http://192.168.56.101:8080 -u administrator -p password -m POST -ct "application/json" /datasource/bl/tables < my_table.json'
+    cmd = 'opal rest -o http://192.168.56.101:8080 --user administrator --password password -m POST -ct "application/json" /datasource/bl/tables < temp/'+table_name+'_table.json'
     #print cmd
-    
-    #doesnt work. givse a 500 error.
-    cmd = 'opal rest -o http://192.168.56.101:8080 -u administrator -p password -m POST -ct "application/json" /datasource/bl/tables < temp/'+table_name+'.json'
-    print cmd
     os.system(cmd)
 
     #######################
     #Now do the variables
+    print 'Setting up the variables'
     content_var = {
         "name": "content",
         "entityType": "Book",
@@ -149,73 +149,80 @@ for this_book in books:
     this_variable_json_name = 'temp/'+table_name+'_vars.json'
     with open(this_variable_json_name, 'w') as fp:
         json.dump(all_vars, fp)
-        
-    #opal rest -o http://localhost:8080 -u administrator -p password -m POST -ct "application/json" /datasource/my_project/table/my_table/variables < my_variables.json
-    #cmd = 'opal rest -o http://192.168.56.101:8080 -u administrator -p password -m POST -ct "application/json" /datasource/bl/table/my_table/variables < my_variables.json'
-    cmd = 'opal rest -o http://192.168.56.101:8080 -u administrator -p password -m POST -ct "application/json" /datasource/bl/table/'+table_name+'/variables < '+this_variable_json_name
+    
+    cmd = 'opal rest -o http://192.168.56.101:8080 --user administrator --password password -m POST -ct "application/json" /datasource/'+project+'/table/'+table_name+'/variables < '+this_variable_json_name
     os.system(cmd)
     
-    
-exit()
+    ###############################################################
+    #Now process the XML files that represent each page in the book
+    #and turn it into a CSV file
+    print 'Processing XML files'
 
-#Get the list of files in the directory
-pages = os.listdir('data')
+    #Get the list of files in the directory
+    pages = os.listdir('data')
 
-#Set the output file
-with open('data.csv','wb') as csvfile:
-    string_file = csv.writer(csvfile)
+    #Set the output file
+    #table_name = 'hamlet'
+    output_csv_file = 'temp/'+table_name+'.csv'
+    with open(output_csv_file,'wb') as csvfile:
+        string_file = csv.writer(csvfile)
 
-    #opal likes a header in its csv file imports
-    string_file.writerow(['id', 'content', 'clean_content', 'hpos', 'vpos', 'width', 'height', 'wc', 'cc', 'page'])
-
-    #cycle through each page
-    for this_page in pages:
-        print this_page
+        #opal likes a header in its csv file imports
+        string_file.writerow(['id', 'content', 'clean_content', 'hpos', 'vpos', 'width', 'height', 'wc', 'cc', 'page'])
         
-        #Parse the xml file
-        tree = ET.parse('data/'+this_page)
+        #cycle through each page
+        for this_page in pages:
+            print this_page
         
-        #Set a root
-        root = tree.getroot()
-        
-        #Grab the page number
-        page = root[2][0]
-        page_number = page.get('ID')
+            #Parse the xml file
+            tree = ET.parse('data/'+this_page)
+            
+            #Set a root
+            root = tree.getroot()
+            
+            #Grab the page number
+            page = root[2][0]
+            page_number = page.get('ID')
+            
+            #The page number starts with a P, get rid of this
+            page_number = page_number.strip('P')
+            
+            #Cycle through the root from the
+            #<Layout><Page><PrintSpace> part. Assume these line up with [2][0][4]
+            for this_textblock in root[2][0][4].findall('TextBlock'):
+                for this_textline in this_textblock.findall('TextLine'):
+                    for this_string in this_textline.findall('String'):
+                        id = this_string.get('ID')
+                        hpos = this_string.get('HPOS')
+                        vpos = this_string.get('VPOS')
+                        width = this_string.get('WIDTH')
+                        height = this_string.get('HEIGHT')
+                        content = this_string.get('CONTENT')
+                        wc = this_string.get('WC')
+                        cc = this_string.get('CC')
+                        
+                        #Clean the content, i.e. the actual words. 
+                        #Lets make it all lower case.
+                        clean_content = content.lower()
+                        
+                        #Get rid of punctuation
+                        clean_content = clean_content.strip('.,!?";:-')
+                        
+                        #Output the information to screen and file
+                        #print content, clean_content, id, hpos, vpos, width, height, wc, cc, page_number
+                        string_file.writerow([id, content.encode('utf-8'), clean_content.encode('utf-8'), hpos, vpos, width, height, wc, cc, page_number])
 
-        #The page number starts with a P, get rid of this
-        page_number = page_number.strip('P')
-        
-        #Cycle through the root from the
-        #<Layout><Page><PrintSpace> part. Assume these line up with [2][0][4]
-        for this_textblock in root[2][0][4].findall('TextBlock'):
-            for this_textline in this_textblock.findall('TextLine'):
-                for this_string in this_textline.findall('String'):
-                    id = this_string.get('ID')
-                    hpos = this_string.get('HPOS')
-                    vpos = this_string.get('VPOS')
-                    width = this_string.get('WIDTH')
-                    height = this_string.get('HEIGHT')
-                    content = this_string.get('CONTENT')
-                    wc = this_string.get('WC')
-                    cc = this_string.get('CC')
-                    
-                    #Clean the content, i.e. the actual words. 
-                    #Lets make it all lower case.
-                    clean_content = content.lower()
-                    
-                    #Get rid of punctuation
-                    clean_content = clean_content.strip('.,!?";:-')
+    ################################
+    #upload the data file
+    print 'Uploading data file'
+    #opal file --opal http://192.168.56.101:8080 --user administrator --password password -up hamlet.csv /home/administrator
+    cmd = 'opal file --opal http://192.168.56.101:8080 --user administrator --password password -up '+output_csv_file+' /home/administrator'
+    os.system(cmd)
 
-                    #Output the information to screen and file
-                    print content, clean_content, id, hpos, vpos, width, height, wc, cc, page_number
-                    string_file.writerow([id, content.encode('utf-8'), clean_content.encode('utf-8'), hpos, vpos, width, height, wc, cc, page_number])
-
-
-#Need to extend these commands
-
-#upload the data file
-#opal file --opal http://192.168.56.101:8080 --user administrator --password password -up hamlet.csv /home/administrator
-
-#import the data
-#opal import-csv -o http://192.168.56.101:8080 -u administrator -p password -d bl --path /home/administrator/data.csv --type Book -v
+    ###############################
+    #import the data
+    print 'Importing the data'
+    #opal import-csv -o http://192.168.56.101:8080 -u administrator -p password -d bl --path /home/administrator/data.csv --type Book -v
+    cmd = 'opal import-csv -o http://192.168.56.101:8080 --user administrator --password password --destination '+project+' --table '+table_name+' --path /home/administrator/hamlet.csv --type Book'
+    os.system(cmd)
 
